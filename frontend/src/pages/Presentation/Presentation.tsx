d04e991a00fea22dd3f52df6dfac11b5820af1c9
Sean Tsai <aleetsaiya@gmail.com>
2024-11-08 12:00:59 +1100

    add features in presentation pages

--- frontend/src/pages/Presentation/Presentation.tsx
+++ frontend/src/pages/Presentation/Presentation.tsx
@@ -1,9 +1,95 @@
-import { useParams } from "react-router-dom";
-import { Typography } from "@mui/material";
+import { useParams, useNavigate, Navigate } from 'react-router-dom';
+import { useState } from 'react';
+import { Box, Paper } from '@mui/material';
+import Navbar from './Navbar';
+import DeletePresentationModal from './DeletePresentationModal';
+import Sidebar from './Sidebar';
+import { useStore } from '../../hooks/useStore';
+import { toast } from 'react-toastify';
 
 const Presentation = () => {
-  const { id } = useParams();
-  return <Typography variant="h2">This is presentation page: {id}</Typography>;
+  const params = useParams();
+  const id = params.id as string;
+  const [openModal, setOpenModal] = useState(false);
+  const navigate = useNavigate();
+  const store = useStore();
+  const sidebarWidth = 280;
+
+  const handleDeletePresentation = async () => {
+    if (!id) {
+      return;
+    }
+    try {
+      await store.deletePresentation(id);
+      toast.success('Delete presentation successfully ');
+      setOpenModal(false);
+      navigate('/dashboard');
+    } catch (err) {
+      toast.error('Fail to delete presentation');
+    }
+  };
+
+  const handleOpenModal = () => {
+    setOpenModal(true);
+  };
+
+  const handleCloseModal = () => {
+    setOpenModal(false);
+  };
+
+  if (!store.isLoading && !Object.keys(store.store).includes(id)) {
+    toast.error('Invalud url');
+    return <Navigate to="/dashboard" />;
+  }
+
+  return (
+    <>
+      <DeletePresentationModal
+        open={openModal}
+        onDelete={handleDeletePresentation}
+        onClose={handleCloseModal}
+      />
+      <Box
+        sx={{
+          overflow: 'auto',
+          maxHeight: '100vh',
+          height: '100vh',
+          display: 'flex',
+          flexDirection: 'column',
+        }}
+      >
+        <Navbar onDeletePresentation={handleOpenModal} />
+        <Box
+          sx={{
+            display: 'flex',
+            flex: '1 1 auto',
+          }}
+        >
+          <Sidebar width={sidebarWidth} />
+          <Box
+            sx={{
+              flex: '1 1 auto',
+              position: 'relative',
+            }}
+          >
+            <Paper
+              sx={{
+                position: 'absolute',
+                top: '50%',
+                left: '50%',
+                width: '95%',
+                height: '95%',
+                transform: 'translate(-50%, -50%)'
+              }}
+              elevation={4}
+            >
+              Inside Paper
+            </Paper>
+          </Box>
+        </Box>
+      </Box>
+    </>
+  );
 };
 
 export default Presentation;
