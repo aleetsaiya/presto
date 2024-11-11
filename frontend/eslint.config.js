57c291d3b443129ee876167105c4e293d421ca85
Sean Tsai <aleetsaiya@gmail.com>
2024-11-11 17:53:11 +1100

    fix linting error: eslint.config.js does not provide react version

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -8,6 +8,7 @@ import tseslint from "typescript-eslint";
 export default tseslint.config(
   { ignores: ["dist", "src/__test__", "src/main.tsx"] },
   {
+    settings: { react: { version: 'detect' } },
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ["**/*.{ts,tsx}"],
     languageOptions: {
