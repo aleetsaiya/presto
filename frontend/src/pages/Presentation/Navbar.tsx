c4500f13fa384c615ffde9ae2de133ecad7d9427
Sean Tsai <aleetsaiya@gmail.com>
2024-11-11 14:51:17 +1100

    create responsive navbar

--- frontend/src/pages/Presentation/Navbar.tsx
+++ frontend/src/pages/Presentation/Navbar.tsx
@@ -1,17 +1,43 @@
-import { Box, Button, Typography } from '@mui/material';
+import { useState } from 'react';
+import {
+  Box,
+  Button,
+  Drawer,
+  IconButton,
+  Toolbar,
+  Typography,
+} from '@mui/material';
 import { useNavigate } from 'react-router-dom';
-import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import {
+  Menu as MenuIcon,
+  ArrowBack as ArrowBackIcon,
+} from '@mui/icons-material';
 import NavbarContainer from '../../components/Navbar';
 import LogoutBtn from '../../components/LogoutBtn';
 import { useTheme } from '@mui/material';
 
 type NavbarProps = {
   onDeletePresentation: React.MouseEventHandler<HTMLButtonElement>;
+  handleShowSidebar: () => void;
 };
 
-const Navbar = ({ onDeletePresentation }: NavbarProps) => {
+const Navbar = ({ onDeletePresentation, handleShowSidebar }: NavbarProps) => {
   const theme = useTheme();
   const navigate = useNavigate();
+  const [showMenu, setShowMenu] = useState(false);
+
+  const handleShowMenu = () => {
+    setShowMenu(true);
+  };
+
+  const handleCloseMenu = () => {
+    setShowMenu(false);
+  };
+
+  const handleClickControlsBtn = () => {
+    handleShowSidebar();
+    handleCloseMenu();
+  };
 
   const BackLink = (
     <Box
@@ -41,24 +67,85 @@ const Navbar = ({ onDeletePresentation }: NavbarProps) => {
     </Box>
   );
 
-  return (
-    <NavbarContainer
-      brandElement={BackLink}
-      theme="dark"
-      sx={{
-        zIndex: (theme) => theme.zIndex.drawer + 1,
-      }}
-    >
+  const controlButtons = (
+    <>
       <Button
         color="error"
         variant="contained"
         onClick={onDeletePresentation}
-        sx={{ textTransform: 'none' }}
+        sx={{
+          textTransform: 'none',
+          borderRadius: {
+            xs: 0,
+            md: 1,
+          },
+        }}
       >
         Delete Presentation
       </Button>
-      <LogoutBtn sx={{ ml: 2 }} />
-    </NavbarContainer>
+      <Button
+        color="inherit"
+        onClick={handleClickControlsBtn}
+        sx={{
+          textTransform: 'none',
+          display: {
+            xs: 'block',
+            md: 'none',
+          },
+          ml: 0,
+          mt: {
+            xs: 1,
+            md: 0,
+          },
+        }}
+      >
+        Controls
+      </Button>
+      <LogoutBtn
+        sx={{
+          ml: {
+            xs: 0,
+            md: 2,
+          },
+          mt: {
+            xs: 1,
+            md: 0,
+          },
+        }}
+      />
+    </>
+  );
+
+  return (
+    <>
+      <NavbarContainer
+        brandElement={BackLink}
+        theme="dark"
+        sx={{
+          zIndex: (theme) => theme.zIndex.drawer + 1,
+        }}
+      >
+        <IconButton
+          onClick={handleShowMenu}
+          color="inherit"
+          sx={{
+            display: {
+              xs: 'block',
+              md: 'none',
+            },
+          }}
+        >
+          <MenuIcon />
+        </IconButton>
+        <Box sx={{ display: { xs: 'none', md: 'block' } }}>
+          {controlButtons}
+        </Box>
+        <Drawer anchor="right" open={showMenu} onClose={handleCloseMenu}>
+          <Toolbar />
+          {controlButtons}
+        </Drawer>
+      </NavbarContainer>
+    </>
   );
 };
 
